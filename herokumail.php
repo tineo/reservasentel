7160a314b2d8582524f5689de649edb917e3869e
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2016-12-23 16:26:51 -0500

    Test Mailing SMTP in Heroku

---
+++ herokumail.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tineo
+ * Date: 23/12/16
+ * Time: 04:25 PM
+ */
+include_once 'testmail.php';
+sendme(1, 1, 1, 1, array("cesar@tineo.mobi"), array("itsudatte01@gmail.com"));
